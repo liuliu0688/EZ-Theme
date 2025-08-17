2a77dfac21b2bd2dcf79a660699e4fca538070db
liuliu0688 <liambakerrhkeit3n@gmail.com>
2025-08-17 14:58:32 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -26,9 +26,9 @@ export const config  = {
         // 支持字符串形式(单个API地址)或数组形式(多个备选API地址)
         // 多个地址时，会按顺序检测可用性，并使用第一个可用的地址
         staticBaseUrl: [
-            'https://bb.beikeyyds888.cc/api/v1',
-            'https://bb.beikeyyds888.cc/api/v1',
-            'https://bb.beikeyyds888.cc/api/v1'
+            'https://beihhdd.liuliuhan.cfd/api/v1',
+            'https://beihhdd.liuliuhan.cfd/api/v1',
+            'https://beihhdd.liuliuhan.cfd/api/v1'
         ],
 
         // 自动获取模式配置 (urlMode = 'auto'时使用)
@@ -49,7 +49,7 @@ export const config  = {
     API_MIDDLEWARE_ENABLED: true,
 
     // 中间件服务器URL (不含路径)
-    API_MIDDLEWARE_URL: 'https://bb.beikeyyds888.cc',
+    API_MIDDLEWARE_URL: 'beihhdd.liuliuhan.cfd',
 
     // 中间件路由前缀 (与中间件服务器配置保持一致)
     API_MIDDLEWARE_PATH: '/api/v1',
